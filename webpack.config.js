8d6ea90f1115058447929b55c402cbbd659e1423
Lyndon Shi <shilyndon@gmail.com>
2019-09-08 22:13:22 -0400

    use darkly bootstrap theme successfully

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        { loader: 'style-loader' },
+        { loader: 'css-loader' },
+        {
+          loader: 'postcss-loader',
+          options: {
+            plugins() {
+              return [
+                require('precss'),
+                require('autoprefixer')
+              ];
+            },
+          },
+        },
+        { loader: 'sass-loader' },
+      ],
+    }],
+  },
+};
