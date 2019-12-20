e758260dad21520d314fc25884b88ef457118919
Lyndon Shi <shilyndon@gmail.com>
2019-12-20 16:17:36 -0500

    add resume page

---
+++ src/pages/resume.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Container from 'react-bootstrap/Container'
+
+const Resume = () => (
+    <Container>
+        <center>
+            <iframe src="https://drive.google.com/file/d/1ZPBlG9RXX9-cW5WLZ6PoRG4vQcm1fPRC/preview" width="100%" height="800vh"></iframe>
+        </center>
+    </Container>
+)
+
+export default Resume
