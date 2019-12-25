b8fdc075b29ea3744bd33d595603019e640fb234
Lyndon Shi <shilyndon@gmail.com>
2019-12-25 12:46:21 -0500

    override bootswatch navbar to get transparency

--- src/components/header.js
+++ src/components/header.js
@@ -3,20 +3,48 @@ import PropTypes from "prop-types";
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 
-const Header = ({ siteTitle }) => (
-    <Navbar bg="light" expand="lg">
-        <Navbar.Brand>
-            <Link to="/" className="brandText">{siteTitle}</Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/resume/">Resume</Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
-);
+class Header extends React.Component {
+    state = {
+        isTop: true
+    };
+
+    constructor(props) {
+        super(props);
+        this.siteTitle = props.siteTitle;
+        this.scrollListener = this.scrollListener.bind(this);
+    }
+
+    scrollListener() {
+        const isTop = window.scrollY < 100;
+        if (isTop !== this.state.isTop) {
+            this.setState({ isTop });
+        }
+        console.log("scrolled");
+    }
+
+    componentDidMount() {
+        window.addEventListener("scroll", this.scrollListener);
+    }
+
+    render() {
+        return (
+            <Navbar bg={this.state.isTop ? "" : "light"} expand="lg" fixed="top" className={"navbarTop " + (this.state.isTop ? "" : "navbarScrolled")}>
+                <Navbar.Brand>
+                    <Link to="/" className="brandText">
+                        {this.siteTitle}
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/" className="navLink">Home</Nav.Link>
+                        <Nav.Link href="/resume/" className="navLink">Resume</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
+}
 
 Header.propTypes = {
     siteTitle: PropTypes.string
