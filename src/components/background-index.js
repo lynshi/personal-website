a720f5041792acb68c644f202e25110a73e93faa
Lyndon Shi <shilyndon@gmail.com>
2019-12-25 14:39:54 -0500

    added typing animation!

--- src/components/background-index.js
+++ src/components/background-index.js
@@ -3,10 +3,12 @@ import { graphql, StaticQuery } from "gatsby";
 import styled from "styled-components";
 
 import BackgroundImage from "gatsby-background-image";
+
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+
+import Typing from "react-typing-animation";
 
 const IndexBackground = ({ className }) => (
     <StaticQuery
@@ -33,9 +35,26 @@ const IndexBackground = ({ className }) => (
                 >
                     <Jumbotron className="indexJumbotron">
                         <Container>
-                            <Row><br></br></Row>
                             <Row>
-                                <h1>Hi, I'm Lyndon.</h1>
+                                <br></br>
+                            </Row>
+                            <Row>
+                                <Typing className="indexTyping">
+                                    <h1>Hi, I'm Lyndon.</h1>
+                                    <h2>Developer | CS @ UMich</h2>
+                                </Typing>
+                            </Row>
+                            <Row>
+                                <Typing className="indexTyping">
+                                    <Typing.Delay ms={3100} />
+                                    <h2>Machine Learning | Algorithms</h2>
+                                </Typing>
+                            </Row>
+                            <Row>
+                                <Typing className="indexTyping">
+                                    <Typing.Delay ms={3100} />
+                                    <h2>Smash Ultimate | Fantasy Football</h2>
+                                </Typing>
                             </Row>
                         </Container>
                     </Jumbotron>
