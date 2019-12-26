33f0abc528480a61a035c04bfb48446efd0ac5b2
Lyndon Shi <shilyndon@gmail.com>
2019-12-25 19:15:30 -0500

    use h3 and remove indent

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,48 +3,26 @@ import PropTypes from "prop-types";
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 
-class Navigation extends React.Component {
-    state = {
-        isTop: true
-    };
-
-    constructor(props) {
-        super(props);
-        this.siteTitle = props.siteTitle;
-        this.scrollListener = this.scrollListener.bind(this);
-    }
-
-    scrollListener() {
-        const isTop = window.scrollY < 10;
-        if (isTop !== this.state.isTop) {
-            this.setState({ isTop });
-        }
-        console.log("scrolled");
-    }
-
-    componentDidMount() {
-        window.addEventListener("scroll", this.scrollListener);
-    }
-
-    render() {
-        return (
-            <Navbar bg={this.state.isTop ? "" : "light"} expand="lg" fixed="top" className={"navbarTop " + (this.state.isTop ? "" : "navbarScrolled")}>
-                <Navbar.Brand>
-                    <Link to="/" className="brandText">
-                        {this.siteTitle}
-                    </Link>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/" className="navLink">Home</Nav.Link>
-                        <Nav.Link href="/resume/" className="navLink">Resume</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        );
-    }
-}
+const Navigation = ({ siteTitle }) => (
+    <Navbar bg="light" expand="lg" fixed="top" className="navbarTop">
+        <Navbar.Brand>
+            <Link to="/" className="brandText">
+                {siteTitle}
+            </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="/" className="navLink">
+                    Home
+                </Nav.Link>
+                <Nav.Link href="/resume/" className="navLink">
+                    Resume
+                </Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+);
 
 Navigation.propTypes = {
     siteTitle: PropTypes.string
