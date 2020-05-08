89ad867829b5f6bc82dee5476328e4396e70484e
Lyndon Shi <shilyndon@gmail.com>
2020-05-07 21:24:17 -0700

    fix resume iframe sizing

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -3,19 +3,20 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 const Resume = () => (
     <Layout>
         <SEO title="Resume" />
         <Container className="marginForNavbar">
-            <center>
-                <iframe
-                    src="https://drive.google.com/file/d/1ZPBlG9RXX9-cW5WLZ6PoRG4vQcm1fPRC/preview"
-                    width="100%"
-                    height="800vh"
-                    title="resume"
-                ></iframe>
-            </center>
+            <Row className="resume">
+                <center>
+                    <iframe
+                        src="https://drive.google.com/file/d/1ZPBlG9RXX9-cW5WLZ6PoRG4vQcm1fPRC/preview"
+                        title="resume"
+                    ></iframe>
+                </center>
+            </Row>
         </Container>
     </Layout>
 );
